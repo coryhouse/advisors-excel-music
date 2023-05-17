deba36f8a733006e2db80499ec40016e8375f73d
Cory House <housecor@gmail.com>
2023-05-17 09:36:44 -0500

    add

--- src/services/songService.ts
+++ src/services/songService.ts
@@ -1,6 +1,79 @@
 import ky from "ky";
 import { Song } from "../types/Song";
 
+type SongResponse = {
+  type: "songs";
+  id: string;
+  attributes: {
+    createdBy: string;
+    createdAt: string;
+    updatedBy: string;
+    updatedAt: string;
+    artist: string;
+    title: string;
+    length: string;
+  };
+  relationships?: {
+    album: {
+      data: {
+        type: "album";
+        id: string;
+      };
+    };
+    playlists?: {
+      data: [
+        {
+          type: "playlists";
+          id: string;
+        }
+      ];
+    };
+    likes?: {
+      data: [
+        {
+          type: "likes";
+          id: string;
+        }
+      ];
+    };
+  };
+};
+
+export type GetSongsResponse = {
+  data: SongResponse[];
+  included?: [
+    {
+      type: string;
+      id: string;
+      attributes: {
+        createdBy: string;
+        createdAt: string;
+        updatedBy: string;
+        updatedAt: string;
+        name: string;
+      };
+      relationships: {
+        songs: {
+          data: [
+            {
+              type: string;
+              id: string;
+            }
+          ];
+        };
+      };
+    }
+  ];
+  links?: {
+    first: string;
+    last: string;
+    prev: string;
+    next: string;
+  };
+};
+
 export async function getSongs(): Promise<Song[]> {
-  return ky("http://localhost:3000/v1/songs").json();
+  const resp = (await ky(
+    "http://localhost:3000/v1/songs"
+  ).json()) as GetSongsResponse;
 }
